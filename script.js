0db521ec958e396e205710955de4929fa3056f7f
Sahar-Mgh <sahar.emma.m@gmail.com>
2024-01-13 15:50:10 +0330

    added fontawesome

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let toastBox = document.getElementById('toastBox');
+let successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully Submitted!';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the Error!';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again!!';
 
-function showToast(){
+function showToast(msg){
     let toast = document.createElement('div');
     toast.classList.add('toast');
-    toast.innerHTML='Success';
+    toast.innerHTML = msg;
     toastBox.appendChild(toast);
 }
